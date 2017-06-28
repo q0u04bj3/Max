06b28aec38dcb0a174f951de3e2945abaf556ae3
Pan Yan Ru <q0u04bj3@gmail.com>
2017-06-28 18:03:22 +0800

    Update Max.py

--- Max.py
+++ Max.py
@@ -6,4 +6,9 @@ with open("sample1.dat") as file:
         if len(lines) > 3:
             lines.remove(min(lines))
 
+for i in range(len(lines)):
+    for i in range(len(lines)-1):
+        if lines[i] < lines[i+1]:
+            lines[i+1], lines[i] = lines[i], lines[i+1]
+
 print '\n'.join(lines)
