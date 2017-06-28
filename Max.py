8237958c4a2970c422e8fab5b1211b2073445b2f
Pan Yan Ru <q0u04bj3@gmail.com>
2017-06-28 17:39:58 +0800

    Update Max.py

--- Max.py
+++ Max.py
@@ -1,7 +1,9 @@
-lines = []
+lines = [0]
 with open("sample1.dat") as file:
     for line in file:
         line = line.strip()
         lines.append(line)
+        if len(lines) > 3:
+            lines.remove(min(lines))
 
-print '\n'.join(sorted(lines,reverse=True)[:3])
+print '\n'.join(lines)
