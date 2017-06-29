b4e95da5eecb89f0d1cf41ee55cdd851acb47f41
Pan Yan Ru <q0u04bj3@gmail.com>
2017-06-29 10:08:33 +0800

    Update Max.py

--- Max.py
+++ Max.py
@@ -1,14 +1,17 @@
 lines = [0]
+
+def my_min(lst):
+    min_val = lst[0]
+    for item in lst:
+        if item < min_val:
+            min_val = item
+    return min_val
+
 with open("sample1.dat") as file:
     for line in file:
         line = line.strip()
         lines.append(line)
         if len(lines) > 3:
-            lines.remove(min(lines))
-
-for i in range(len(lines)):
-    for i in range(len(lines)-1):
-        if lines[i] < lines[i+1]:
-            lines[i+1], lines[i] = lines[i], lines[i+1]
+            lines.remove(my_min(lines))
 
 print '\n'.join(lines)
