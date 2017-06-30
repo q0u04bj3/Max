8cab9e151cc04bf1dc66141c8cea27022ba735b8
Pan Yan Ru <q0u04bj3@gmail.com>
2017-06-30 17:42:53 +0800

    Update test_Max.py

--- test_Max.py
+++ test_Max.py
@@ -3,11 +3,31 @@ from Max import Max,my_min
 
 class PerfectNumTest(unittest.TestCase):
     
-    def test_Max(self):
-        self.assertEqual(Max("sample1.dat"), '6421\n8723\n9239')
-        
-    def test_my_min(self):
-        self.assertEqual(my_min([1,5,100]), 1)
+    def test_Max1(self):
+        expected_value = ['6421', '8723', '9239']
+        test_file="sample1.dat"
+        value = Max(test_file)
+        self.assertEqual(value, expected_value)
+
+    def test_Max2(self):
+        expected_value = ['9867667676', '98676', '9793764']
+        test_file="sample2.dat"
+        value = Max(test_file)
+        self.assertEqual(value, expected_value)
+
+    def test_Max3(self):
+        expected_value = ['1111111111111111111111111', '11111111111111111111111111', '111111111111111111111111111']
+        test_file="sample3.dat"
+        value = Max(test_file)
+        self.assertEqual(value, expected_value)
+
+    def test_Max4(self):
+        expected_value = ['700','800','900']
+        test_file="sample4.dat"
+        value = Max(test_file)
+        self.assertEqual(value, expected_value)
+
+
 
 if __name__ == '__main__':
     unittest.main()
