6190565a5bd9b4a2e89b84e66fcdc5ff69fe841c
Pan Yan Ru <q0u04bj3@gmail.com>
2017-06-30 11:25:45 +0800

    Update test_Max.py

--- test_Max.py
+++ test_Max.py
@@ -5,6 +5,9 @@ class PerfectNumTest(unittest.TestCase):
     
     def test_Max(self):
         self.assertEqual(Max("sample1.dat"), '6421\n8723\n9239')
+        
+    def test_my_min(self):
+        self.assertEqual(my_min([1,5,100]), 1)
 
 if __name__ == '__main__':
     unittest.main()
