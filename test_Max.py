27832708c153af77c59e5e653f8aadea52568e33
Pan Yan Ru <q0u04bj3@gmail.com>
2017-07-03 09:55:56 +0800

    Update test_Max.py

--- test_Max.py
+++ test_Max.py
@@ -1,33 +1,31 @@
 import unittest
-from Max import Max,my_min
+from Max import readFile, my_3max
 
 class PerfectNumTest(unittest.TestCase):
     
-    def test_Max1(self):
-        expected_value = ['6421', '8723', '9239']
+    def test_readFile(self):
+        expected_value = ['2134', '3412', '6421', '8723', '9239', '1234', '2345']
         test_file="sample1.dat"
-        value = Max(test_file)
+        value = readFile(test_file)
         self.assertEqual(value, expected_value)
 
-    def test_Max2(self):
-        expected_value = ['9867667676', '98676', '9793764']
-        test_file="sample2.dat"
-        value = Max(test_file)
+    def test_my_3max_num(self):
+        expected_value = [8, 9, 10]
+        test_value=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        value = my_3max(test_value)
+        self.assertEqual(value, expected_value)
+    
+    def test_my_3max_zero(self):
+        expected_value = [0, 0, 0]
+        test_value=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        value = my_3max(test_value)
         self.assertEqual(value, expected_value)
 
-    def test_Max3(self):
-        expected_value = ['1111111111111111111111111', '11111111111111111111111111', '111111111111111111111111111']
-        test_file="sample3.dat"
-        value = Max(test_file)
+    def test_my_3max_none(self):
+        expected_value = []
+        test_value=[]
+        value = my_3max(test_value)
         self.assertEqual(value, expected_value)
-
-    def test_Max4(self):
-        expected_value = ['']
-        test_file="sample4.dat"
-        value = Max(test_file)
-        self.assertEqual(value, expected_value)
-
-
 
 if __name__ == '__main__':
     unittest.main()
